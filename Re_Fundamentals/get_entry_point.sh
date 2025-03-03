64ee9ba2db766cb1f6306603df115f4071d96851
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2025-03-03 20:41:33 +0000

    Byte Order fix.

--- Re_Fundamentals/get_entry_point.sh
+++ Re_Fundamentals/get_entry_point.sh
@@ -14,6 +14,6 @@ fi
 file_name=$0
 magic_number=$(echo $ELF | sed -nE 's/^.*Magic: (.*) Class.*/\1/p')
 class=$(echo $ELF | sed -nE 's/^.*Class: (.*) Data:.*/\1/p')
-byte_order=$(echo $ELF | sed -nE 's/^.*Data: (.* endian) .*/\1/p')
+byte_order=$(echo $ELF | sed -nE "s/^.*Data: .*, (.* endian) .*/\1/p")
 entry_point_address=$(echo $ELF | sed -nE 's/^.*Entry point address: (0x[0-9]+) .*/\1/p')
 display_elf_header_info
